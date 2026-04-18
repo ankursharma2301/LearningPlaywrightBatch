384ec85f9f9e987c8939347fe71fb93df187a5e6
Ankur <ankursharma2301@gmail.com>
2026-04-18 20:31:26 +0530

    feat: Initialize Playwright project with TypeScript in Chapter 23 and fix config errors

--- Practice_Programs/Classes and Functions wit type.ts
+++ Practice_Programs/Classes and Functions wit type.ts
@@ -1,46 +1,124 @@
-// class User {
-//     public username: string
-//     private password: string
+// // // // // // // class User {
+// // // // // // //     public username: string
+// // // // // // //     private password: string
 
-//     constructor(username: string, password: string) {
-//         this.username = username;
-//         this.password = password;
+// // // // // // //     constructor(username: string, password: string) {
+// // // // // // //         this.username = username;
+// // // // // // //         this.password = password;
+// // // // // // //     }
+
+// // // // // // //     login(): string {
+// // // // // // //         if (this.password === "1234") {
+// // // // // // //             return "Login Success"
+// // // // // // //         } else {
+// // // // // // //             return "Login Failed"
+// // // // // // //         }
+// // // // // // //     }
+// // // // // // //     getMaskedPassword(): string {
+// // // // // // //         return "*".repeat(this.password.length)
+
+// // // // // // //     }
+// // // // // // // }
+
+// // // // // // // let response = new User("Ankur", "1234");
+
+// // // // // // // console.log(response.login());
+
+// // // // // // // console.log(response.getMaskedPassword());
+
+// // // // // // class BasePage {
+// // // // // //     protected url: string;
+
+// // // // // //     constructor(url: string) {
+// // // // // //         this.url = url;
+// // // // // //     }
+// // // // // // }
+
+// // // // // // class LoginPage extends BasePage {
+// // // // // //     openPage(): string {
+// // // // // //         return `Opening ${this.url}`
+
+// // // // // //     }
+// // // // // // }
+
+// // // // // // let openUrl = new LoginPage("https://google.com/login");
+
+// // // // // // console.log(openUrl.openPage());
+// // // // // ✅ Question 4 (Basic Function)
+
+// // // // // 👉 Function banao:
+
+// // // // // multiply(a: number, b: number): number
+
+// // // // function multiply(a: number, b: number): number {
+// // // //     return a * b;
+// // // // }
+
+// // // // console.log(`Multiplication of two number is :${multiply(2, 3)}`);
+
+// // // function checkStatus(status: number | string): string {
+// // //     return `Status is : ${status}`;
+
+// // // }
+
+// // // console.log(checkStatus("200"));
+// // // console.log(checkStatus("OK"));
+// // ✅ Question 6 (Real QA Logic)
+
+// // 👉 Function:
+
+// // validateLogin(token?: string): string
+
+// // Rules:
+
+// // agar token hai → "Login Success"
+// // nahi hai → "Login Failed"
+
+// function validateLogin(token?: string): string {
+//     if (token) {
+//         return "Login Sucess";
+//     } else {
+//         return "Login Failed";
 //     }
 
-//     login(): string {
-//         if (this.password === "1234") {
-//             return "Login Success"
+// }
+
+// console.log(validateLogin("abcdef"));
+// console.log(validateLogin());
+
+class LoginService {
+    public username: string;
+    public password: string
+
+    constructor(username: string, password: string) {
+        this.username = username;
+        this.password = password;
+    }
+
+    login(): string {
+        if (this.username === "ankursharma2301" && this.password === "admin1234") {
+            return "Success";
+        } else {
+            return "Fail";
+        }
+    }
+}
+
+let checklogin = new LoginService("ankursharma2301", "admin1234")
+
+console.log(checklogin.login());
+
+// class LoginService {
+//     login(username: string, password: string): string {
+//         if (username === "admin" && password === "1234") {
+//             return "Success";
 //         } else {
-//             return "Login Failed"
+//             return "Fail";
 //         }
-//     }
-//     getMaskedPassword(): string {
-//         return "*".repeat(this.password.length)
-
 //     }
 // }
 
-// let response = new User("Ankur", "1234");
+// let checklogin = new LoginService();
 
-// console.log(response.login());
-
-// console.log(response.getMaskedPassword());
-
-class BasePage {
-    protected url: string;
-
-    constructor(url: string) {
-        this.url = url;
-    }
-}
-
-class LoginPage extends BasePage {
-    openPage(): string {
-        return `Opening ${this.url}`
-
-    }
-}
-
-let openUrl = new LoginPage("https://google.com/login");
-
-console.log(openUrl.openPage());
+// console.log(checklogin.login("admin", "1234")); // Success
+// console.log(checklogin.login("user", "0000"));  // Fail
