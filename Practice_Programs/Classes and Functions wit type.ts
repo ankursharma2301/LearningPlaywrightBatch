c65e64cb0162c76da3b379d4cf58b06697349a62
Ankur <ankursharma2301@gmail.com>
2026-04-19 23:13:39 +0530

    Update Classes and Functions with type script

--- Practice_Programs/Classes and Functions wit type.ts
+++ Practice_Programs/Classes and Functions wit type.ts
@@ -1,124 +1,225 @@
-// // // // // // // class User {
-// // // // // // //     public username: string
-// // // // // // //     private password: string
+// // // // // // // // // // class User {
+// // // // // // // // // //     public username: string
+// // // // // // // // // //     private password: string
 
-// // // // // // //     constructor(username: string, password: string) {
-// // // // // // //         this.username = username;
-// // // // // // //         this.password = password;
-// // // // // // //     }
+// // // // // // // // // //     constructor(username: string, password: string) {
+// // // // // // // // // //         this.username = username;
+// // // // // // // // // //         this.password = password;
+// // // // // // // // // //     }
 
-// // // // // // //     login(): string {
-// // // // // // //         if (this.password === "1234") {
-// // // // // // //             return "Login Success"
-// // // // // // //         } else {
-// // // // // // //             return "Login Failed"
-// // // // // // //         }
-// // // // // // //     }
-// // // // // // //     getMaskedPassword(): string {
-// // // // // // //         return "*".repeat(this.password.length)
+// // // // // // // // // //     login(): string {
+// // // // // // // // // //         if (this.password === "1234") {
+// // // // // // // // // //             return "Login Success"
+// // // // // // // // // //         } else {
+// // // // // // // // // //             return "Login Failed"
+// // // // // // // // // //         }
+// // // // // // // // // //     }
+// // // // // // // // // //     getMaskedPassword(): string {
+// // // // // // // // // //         return "*".repeat(this.password.length)
 
-// // // // // // //     }
+// // // // // // // // // //     }
+// // // // // // // // // // }
+
+// // // // // // // // // // let response = new User("Ankur", "1234");
+
+// // // // // // // // // // console.log(response.login());
+
+// // // // // // // // // // console.log(response.getMaskedPassword());
+
+// // // // // // // // // class BasePage {
+// // // // // // // // //     protected url: string;
+
+// // // // // // // // //     constructor(url: string) {
+// // // // // // // // //         this.url = url;
+// // // // // // // // //     }
+// // // // // // // // // }
+
+// // // // // // // // // class LoginPage extends BasePage {
+// // // // // // // // //     openPage(): string {
+// // // // // // // // //         return `Opening ${this.url}`
+
+// // // // // // // // //     }
+// // // // // // // // // }
+
+// // // // // // // // // let openUrl = new LoginPage("https://google.com/login");
+
+// // // // // // // // // console.log(openUrl.openPage());
+// // // // // // // // ✅ Question 4 (Basic Function)
+
+// // // // // // // // 👉 Function banao:
+
+// // // // // // // // multiply(a: number, b: number): number
+
+// // // // // // // function multiply(a: number, b: number): number {
+// // // // // // //     return a * b;
 // // // // // // // }
 
-// // // // // // // let response = new User("Ankur", "1234");
+// // // // // // // console.log(`Multiplication of two number is :${multiply(2, 3)}`);
 
-// // // // // // // console.log(response.login());
+// // // // // // function checkStatus(status: number | string): string {
+// // // // // //     return `Status is : ${status}`;
 
-// // // // // // // console.log(response.getMaskedPassword());
-
-// // // // // // class BasePage {
-// // // // // //     protected url: string;
-
-// // // // // //     constructor(url: string) {
-// // // // // //         this.url = url;
-// // // // // //     }
 // // // // // // }
 
-// // // // // // class LoginPage extends BasePage {
-// // // // // //     openPage(): string {
-// // // // // //         return `Opening ${this.url}`
+// // // // // // console.log(checkStatus("200"));
+// // // // // // console.log(checkStatus("OK"));
+// // // // // ✅ Question 6 (Real QA Logic)
 
-// // // // // //     }
-// // // // // // }
+// // // // // 👉 Function:
 
-// // // // // // let openUrl = new LoginPage("https://google.com/login");
+// // // // // validateLogin(token?: string): string
 
-// // // // // // console.log(openUrl.openPage());
-// // // // // ✅ Question 4 (Basic Function)
+// // // // // Rules:
 
-// // // // // 👉 Function banao:
+// // // // // agar token hai → "Login Success"
+// // // // // nahi hai → "Login Failed"
 
-// // // // // multiply(a: number, b: number): number
+// // // // function validateLogin(token?: string): string {
+// // // //     if (token) {
+// // // //         return "Login Sucess";
+// // // //     } else {
+// // // //         return "Login Failed";
+// // // //     }
 
-// // // // function multiply(a: number, b: number): number {
-// // // //     return a * b;
 // // // // }
 
-// // // // console.log(`Multiplication of two number is :${multiply(2, 3)}`);
+// // // // console.log(validateLogin("abcdef"));
+// // // // console.log(validateLogin());
 
-// // // function checkStatus(status: number | string): string {
-// // //     return `Status is : ${status}`;
+// // // class LoginService {
+// // //     public username: string;
+// // //     public password: string
 
+// // //     constructor(username: string, password: string) {
+// // //         this.username = username;
+// // //         this.password = password;
+// // //     }
+
+// // //     login(): string {
+// // //         if (this.username === "ankursharma2301" && this.password === "admin1234") {
+// // //             return "Success";
+// // //         } else {
+// // //             return "Fail";
+// // //         }
+// // //     }
 // // // }
 
-// // // console.log(checkStatus("200"));
-// // // console.log(checkStatus("OK"));
-// // ✅ Question 6 (Real QA Logic)
+// // // let checklogin = new LoginService("ankursharma2301", "admin1234")
 
-// // 👉 Function:
+// // // console.log(checklogin.login());
 
-// // validateLogin(token?: string): string
+// // // // class LoginService {
+// // // //     login(username: string, password: string): string {
+// // // //         if (username === "admin" && password === "1234") {
+// // // //             return "Success";
+// // // //         } else {
+// // // //             return "Fail";
+// // // //         }
+// // // //     }
+// // // // }
 
-// // Rules:
+// // // // let checklogin = new LoginService();
 
-// // agar token hai → "Login Success"
-// // nahi hai → "Login Failed"
+// // // // console.log(checklogin.login("admin", "1234")); // Success
+// // // // console.log(checklogin.login("user", "0000"));  // Fail
 
-// function validateLogin(token?: string): string {
-//     if (token) {
-//         return "Login Sucess";
-//     } else {
-//         return "Login Failed";
+// // class LoginPage {
+// //     constructor(private page: any) { }
+
+// //     enterUsername(username: string): string {
+// //         return `Enter Username:${username}`;
+// //     }
+// //     enterPassword(password: string): string {
+// //         return `Enter password:${password}`;
+// //     }
+
+// //     clickLogin() {
+// //         return "Click on Logn button"
+// //     }
+
+// //     login(username: string, password: string) {
+// //         this.enterUsername(username);
+// //         this.enterPassword(password);
+// //         this.clickLogin();
+
+// //         if (username === "admin1234" && password === "1234") {
+// //             return "Login Success";
+// //         } else {
+// //             return "Login Failed";
+// //         }
+// //     }
+// // }
+
+
+// class User {
+//     username: string;
+//     role: "admin" | "user";
+//     private isLoggedIn: boolean;
+
+//     constructor(username: string, role: "admin" | "user") {
+//         this.username = username;
+//         this.role = role;
+//         this.isLoggedIn = false; // default
 //     }
 
+//     login(): void {
+//         this.isLoggedIn = true;
+//     }
+
+//     logout(): void {
+//         this.isLoggedIn = false;
+//     }
+
+//     getAccess(): string {
+//         if (!this.isLoggedIn) return "No Access";
+//         return this.role === "admin" ? "Full Access" : "Limited Access";
+//     }
 // }
 
-// console.log(validateLogin("abcdef"));
-// console.log(validateLogin());
+// // Usage
+// let adminuser = new User("Ankur", "admin");
+// console.log(adminuser.getAccess()); // No Access
 
-class LoginService {
-    public username: string;
-    public password: string
+// adminuser.login();
+// console.log(adminuser.getAccess()); // Full Access
 
-    constructor(username: string, password: string) {
-        this.username = username;
-        this.password = password;
+// let normaluser = new User("john", "user");
+// normaluser.login();
+// console.log(normaluser.getAccess()); // Limited Access
+
+
+class RetryHelper {
+    private maxRetry: number
+    constructor(maxRetry: number) {
+        this.maxRetry = maxRetry;
     }
 
-    login(): string {
-        if (this.username === "ankursharma2301" && this.password === "admin1234") {
-            return "Success";
-        } else {
-            return "Fail";
+    retry(taskName: string): string {
+        for (let i = 1; i <= this.maxRetry; i++) {
+            console.log(`Retrying ${taskName}....Attempt ${i}`);
+
         }
+        return `${taskName} completed after ${this.maxRetry} attempts`;
     }
 }
 
-let checklogin = new LoginService("ankursharma2301", "admin1234")
+let retryHelper = new RetryHelper(3);
 
-console.log(checklogin.login());
+console.log(retryHelper.retry("Login Test"));
 
-// class LoginService {
-//     login(username: string, password: string): string {
-//         if (username === "admin" && password === "1234") {
-//             return "Success";
-//         } else {
-//             return "Fail";
-//         }
-//     }
-// }
+//===========================================
+class BasePage {
+    load(): string {
+        return "Load Base Page"
+    }
+}
 
-// let checklogin = new LoginService();
+class DashboardPage extends BasePage {
+    load(): string {
+        return super.load() + " → Dashboard Loaded";
 
-// console.log(checklogin.login("admin", "1234")); // Success
-// console.log(checklogin.login("user", "0000"));  // Fail
+    }
+}
+
+let page = new DashboardPage();
+console.log(page.load());
